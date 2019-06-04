77608da42728dd6746c4aa6f3899c1001bf70495
Chris Efstathiades <chrise3012@icloud.com>
2019-06-04 17:34:12 +0100

    footer and more mobile changes

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,6 +1,8 @@
 
 //all functions
 function init() {
+  //mobile touch effects
+  touchscreen();
   //controls info block functionality
   readMore();
   //controls side menu functionality
@@ -13,13 +15,13 @@ function init() {
 function modals() {
 
   //set display to flex and fade in modal
-  $(document).on('click', '#signup', function(){
+  $(document).on('click', '#signup', () => {
     $(".signup-modal").css("display", "flex")
     $(".signup-modal").fadeTo(200, 1);
   })
 
   //fade out modal and set display to none
-  $(".modal-close").click(function(){
+  $(".modal-close").click(() => {
     $(".signup-modal").fadeTo(200, 0);
     $(".signup-modal").fadeOut()
   })
@@ -34,10 +36,22 @@ function modals() {
 }
 
 
+function touchscreen() {
+  $(".testimonial").on("touchstart", () => {
+    $(this).css({
+      "fontSize":"0px",
+      "border": "0"
+    })
+
+  $(this).css("boxShadow", "inset 0 0 0 0 rgba(0, 0, 0, 0.75)")
+  })
+}
+
+
 function readMore() {
   less = true;
   var info = $(".info p")
-  $(".info span").click(function() {
+  $(".info span").click(() => {
     if (less) {
       info.css({
         'display': 'block'
@@ -62,20 +76,20 @@ function readMore() {
 function sideMenu() {
 
   //open side menu
-  $(".fa-bars").click(function() {
+  $(".fa-bars").click(() => {
     $("#side-menu").css("width", "250px");
     $("#side-menu ul li, #side-menu ul li a").css("opacity", "1");
   })
 
   //close side menu
-  $(".close").click(function() {
+  $(".close").click(() => {
     $("#side-menu").css("width", "0px");
     $("#side-menu ul li, #side-menu ul li a").css("opacity", "0");
   })
 
   //close side menu when outside area is clicked
   $(document).click(function(event) {
-    if (!$(event.target).closest("#side-menu, .fa-bars").length) {
+    if (!$(event.target).closest("#side-menu, .fa-bars, .modal-close").length) {
       $("#side-menu").css("width", "0px");
       $("#side-menu ul li, #side-menu ul li a").css("opacity", "0");
     }
